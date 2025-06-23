f4c7b0db08eb9d78709f2d8efad7db15177b63e6
shamsuttabriz <shamsuttabriz.md@gmail.com>
2025-06-23 15:09:23 +0600

    Google login funnctionality has been implemented

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
 import {
   createUserWithEmailAndPassword,
+  GoogleAuthProvider,
   onAuthStateChanged,
   signInWithEmailAndPassword,
+  signInWithPopup,
   signOut,
 } from "firebase/auth";
 import { auth } from "../Firebase/firebase.init";
+
+const provider = new GoogleAuthProvider();
 
 function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
@@ -21,6 +25,11 @@ function AuthProvider({ children }) {
     setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
+
+  const signInWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, provider);
+  }
 
   const signOutUser = () => {
     setLoading(true);
@@ -43,6 +52,7 @@ function AuthProvider({ children }) {
     loading,
     createUser,
     signInUser,
+    signInWithGoogle,
     signOutUser,
   };
   return <AuthContext value={authInfo}>{children}</AuthContext>;
--- src/pages/Shared/Banner.jsx
+++ src/pages/Shared/Banner.jsx
@@ -4,9 +4,12 @@ import team1 from "../../assets/team/team1.jpg";
 import team2 from "../../assets/team/team2.jpg";
 
 function Banner() {
+
+ 
+
   return (
     <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content bg-red-500 flex-col lg:flex-row-reverse">
+      <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="flex-1">
           <motion.img
             animate={{
@@ -27,8 +30,6 @@ function Banner() {
         </div>
         <div className="flex-1">
           <motion.h1
-            initial={{ scale: 0 }}
-            animate={{ scale: 1, transition: { duration: 2 } }}
             className="text-5xl font-extrabold"
           >
             Find Your{" "}
